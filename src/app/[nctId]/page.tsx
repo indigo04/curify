8d9390d744437cef1ca579e9c461783044674899
Vladyslav Shylin <indiggoo228@gmail.com>
2025-06-06 10:41:45 +0300

    final

--- src/app/[nctId]/page.tsx
+++ src/app/[nctId]/page.tsx
@@ -1,16 +1,16 @@
 import SubmissionForm from "@/components/SubmissionForm";
 
 interface SubmissionPageProps {
-  params: {
+  params: Promise<{
     nctId: string;
-  };
+  }>;
 }
 
 export default async function SubmissionPage({ params }: SubmissionPageProps) {
   const { nctId } = await params;
   return (
     <main className="container mx-auto flex flex-col justify-center items-center h-screen">
-      <SubmissionForm code={nctId}/>
+      <SubmissionForm code={nctId} />
     </main>
   );
 }
